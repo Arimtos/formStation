8f4ca1e04637c02618fab2660e3cf5de921137dd
arimtos <khaled.awashreh.yes@gmail.com>
2019-02-23 15:04:57 +0200

    meep

--- engine/src/engine/ModelReader.java
+++ engine/src/engine/ModelReader.java
@@ -1,11 +1,34 @@
 package engine;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
 public class ModelReader {
+	private static String model;
 	public ModelReader(String model) {
-		
+		this.model=model;
 	}
-	//public String constructModel() {
-		
-	//}
-	
+
+
+
+	public static void lexicalScanner() {
+		List<String> Tokens = new LinkedList<String>(); 
+		model= model.replaceAll("\\s+","");
+		String stop="";
+		while(stop.length()!=model.length()) {
+			String element=model.substring('<', '>');
+			stop=stop+element;
+			Tokens.add(element);
+			model=model.substring(model.indexOf('>'),model.lastIndexOf('>'));
+		}
+         System.out.println("end");
+
+
+
+
+		//public String constructModel() {
+
+		//}
+
+	}
 }
--- engine/src/engine/test.java
+++ engine/src/engine/test.java
@@ -9,5 +9,28 @@ public class test {
 		Engine test=new Engine();
 		Input input=new Input("text","Name","2"," ");
 		System.out.print(input.makeHTML());
+		ModelReader testtwo=new ModelReader("<xforms>\r\n" + 
+				"<model>\r\n" + 
+				"  <instance>\r\n" + 
+				"  <person>\r\n" + 
+				"    <fname/>\r\n" + 
+				"    <lname/>\r\n" + 
+				"  </person>\r\n" + 
+				"  </instance>\r\n" + 
+				"  <submission id=\"form1\" method=\"get\"\r\n" + 
+				"   action=\"submit.asp\"/>\r\n" + 
+				"</model>\r\n" + 
+				"\r\n" + 
+				"<input ref=\"fname\">\r\n" + 
+				"<label>First Name</label></input><br />\r\n" + 
+				"\r\n" + 
+				"<input ref=\"lname\">\r\n" + 
+				"<label>Last Name</label></input><br /><br />\r\n" + 
+				"\r\n" + 
+				"<submit submission=\"form1\">\r\n" + 
+				"<label>Submit</label></submit>\r\n" + 
+				"</xforms>");
+		testtwo.lexicalScanner();
+
 	}
 }
